8a9a116f63e805cd65c1c5fc622ea6ea94558385
Dimitra Be <d.berni@hotmail.com>
2023-04-03 22:44:22 +0300

    Add the header updated with the relevant css classes and add also rules for responsiveness.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,29 @@
 import logo from "../images/Logo.svg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 import Nav from "./Nav";
 
+function clickHamburger(){
+    // const getStatus = document.getElementById("mySidebar").style.display;
+    // if (getStatus == "none"){
+    //     document.getElementById("mySidebar").style.display = 'block';
+    // }else{
+    //     document.getElementById("mySidebar").style.display = 'none';
+    // }
+    console.log('click');
+}
+
+
 function Header(props){
     return(
-        <header>
-            <img src={logo} alt="The liitle lemon logo consists of a yellow lemon on the left and right next to it, it is written 'Little Lemon' in capital letters"/>
-            <Nav/>
+        <header className="header-container">
+            <img src={logo} alt="The little lemon logo consists of a yellow lemon on the left and right next to it, it is written 'Little Lemon' in capital letters"/>
+            <Nav className="flexbox-row dark_gray_text" responsiveness="hide-small hide-medium"/>
+            <a href="#mySidebar" className="hide-large dark_gray_text" onClick={clickHamburger()}>
+                    <FontAwesomeIcon icon={faBars} size="1x"/>
+            </a>
+            <Nav id="mySidebar" className="flexbox-col align-right dark_gray_text animate-right"/>
         </header>
     )
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,13 @@
 function Nav(props){
     return(
-        <nav>
-            <ul>
-                <li><a href="#home">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#Menu">Menu</a></li>
-                <li><a href="#reservations">Reservations</a></li>
-                <li><a href="#order">Order Online</a></li>
-                <li><a href="#login">Login</a></li>
+        <nav className={props.responsiveness} id={props.id}>
+            <ul className={props.className}>
+                <li><h3><a href="#home">Home</a></h3></li>
+                <li><h3><a href="#about">About</a></h3></li>
+                <li><h3><a href="#Menu">Menu</a></h3></li>
+                <li><h3><a href="#reservations">Reservations</a></h3></li>
+                <li><h3><a href="#order">Order Online</a></h3></li>
+                <li><h3><a href="#login">Login</a></h3></li>
             </ul>
         </nav>
     )
