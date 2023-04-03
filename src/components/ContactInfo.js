2324ceffcbf68f80e7da48fd37c376c0d3f17d5e
Dimitra Be <d.berni@hotmail.com>
2023-04-03 22:59:10 +0300

    Add the classes for the footer add props to Contact info

--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -1,10 +1,10 @@
 function ContactInfo(props){
     return(
-        <ul>
-            <li>Contact</li>
-            <li>Imaginary street 12</li>
-            <li>1234567890</li>
-            <li><a href="littlelemon@littlelemon.com">littlelemon@littlelemon.com</a></li>
+        <ul className={props.className}>
+            <li><h3>Contact</h3></li>
+            <li><h3>Imaginary street 12</h3></li>
+            <li><h3>1234567890</h3></li>
+            <li><h3><a href="littlelemon@littlelemon.com">littlelemon@littlelemon.com</a></h3></li>
         </ul>
     )
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,11 +6,13 @@ import SocialMediaIcons from "./SocialMediaIcons";
 
 function Footer(props){
     return(
-        <footer>
-            <img src={logo} alt="The liitle lemon logo consists of a yellow lemon on the left and right next to it, it is written 'Little Lemon' in capital letters"/>
-            <Nav />
-            <ContactInfo />
-            <SocialMediaIcons />
+        <footer className={props.color}>
+            <div className="flex-container white_text align-start">
+                <img src={logo} alt="The little lemon logo consists of a yellow lemon on the left and right next to it, it is written 'Little Lemon' in capital letters"/>
+                <Nav className="flexbox-col"/>
+                <ContactInfo className="flexbox-col"/>
+                <SocialMediaIcons className="flexbox-row wrap"/>
+            </div>
         </footer>
     )
 }
