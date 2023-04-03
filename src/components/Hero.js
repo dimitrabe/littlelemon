b47e62cdc346c05f9376f8dc4ab9364be4b91de2
Dimitra Be <d.berni@hotmail.com>
2023-04-03 22:50:19 +0300

    Add hero section updated with the relevant css classes and also rules for responsiveness

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,19 +1,25 @@
 import fingerfood from "../images/restauranfood.jpg";
 import Button from "./Button";
 
-function Hero(props){
-    return(
-        <div className="heroSection">
-            <h1>Little Lemon</h1>
-            <h2>Chicago</h2>
-            <br/>
-            <p>
-                We are a family-owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.
-            </p>
-            <Button className="myButton primary" value="Reserve a table" link="ReservationOptions"/>
-            <img src={fingerfood} alt="A person is serving food on a big plate" />
+function Hero(props) {
+    return (
+        <div className={`${props.color}`}>
+            <div className="flex-container">
+                <div className="flexbox-col">
+                    <div>
+                        <h1 className="yellow_text">Little Lemon</h1>
+                        <h2 className="white_text">Chicago</h2>
+                    </div>
+                    <p className="white_text">
+                        We are a family-owned Mediterranean restaurant, focused on traditional recipes, served with a modern twist.
+                    </p>
+                    <br/>
+                    <Button className="myButton primary" value="Reserve a table" link="ReservationOptions" />
+                </div>
+
+                <img className="hero-image" src={fingerfood} alt="A person is serving food on a big plate" />
+            </div>
         </div>
     )
 }
-
 export default Hero;
