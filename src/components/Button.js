0d5d8ff9a37d8c5b39bcc61710c7dc938bab7859
Dimitra Be <d.berni@hotmail.com>
2023-04-03 22:40:04 +0300

    Define the style of the buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,6 @@
 function Button(props){
     return(
-        <a href={props.link}>{props.value}</a>
+        <a href={props.link} className={`${props.className} lead-text`}>{props.value}</a>
     )
 }
 export default Button;
