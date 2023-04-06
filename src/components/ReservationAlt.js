4a6fb096d21518c25bc33f242e2e32cbb8741d32
Dimitra Be <d.berni@hotmail.com>
2023-04-06 11:38:37 +0300

    Added the styles in the ReservationAlt and ReservationDetails options. Created an new font style section-text. TBF: the layout of ReservationsDetails is not 100% correct, maybe a grid would be more convenient. Form functionality. Select dropdown styling... Mozilla is ignoring my CSS.

--- src/components/ReservationAlt.js
+++ src/components/ReservationAlt.js
@@ -1,42 +1,52 @@
 import Button from "./Button";
 
 const alternatives = [
-{
-    id               : 28042023,
-    alternativeDate  : "Monday 28th April",
-    alternativeTimes : ["15.00", "16.00", "17.00"]
-},
-{
-    id               : 29042023,
-    alternativeDate : "Tuesday 29th April",
-    alternativeTimes : ["15.00", "16.00", "17.00"]
-}
+    {
+        id: 28042023,
+        alternativeDate: "Monday 28th April",
+        alternativeTimes: ["15.00", "16.00"]
+    },
+    {
+        id: 29042023,
+        alternativeDate: "Tuesday 29th April",
+        alternativeTimes: ["15.00", "16.00", "17.00"]
+    }
 ];
 
 const mappedItems = alternatives.map(theItem => {
     return (
-        <fieldset key={theItem.id}>
+        <fieldset key={theItem.id} className="flexbox-row take-half-the-container space-center">
             {/* find a better key???*/}
-            <legend key={String(theItem.id) + String(1000)}>{theItem.alternativeDate}</legend> 
+            <legend key={String(theItem.id) + String(1000)} className="section-text">{theItem.alternativeDate}</legend>
             {theItem.alternativeTimes.map((theSubItem, index) => (
                 <button key={String(theItem.id) + String(index)} className="myButton options">{theSubItem}</button>
             ))}
         </fieldset>
     )
-  });
+});
 
 
-function ReservationAlt(props){
-    return(
+function ReservationAlt(props) {
+    return (
         <div className="ReservationAltSection">
-            <h2>Unfortunately, availability is limited for the date and time that you have selected.</h2>
-            <h2>Please select an alternative or contact us at 1234567890.</h2>
-            <form>
-                {mappedItems}
-                <Button className="myButton secondary" value="Back" link="ReservationOptions"/>
-                <button type="submit" className="myButton primary">Proceed</button>
-            </form>
-        </div>
+           <div className="flex-container">
+                <div className="flexbox-col take-the-full-container">
+                    <br/>
+                    <br/>
+                    <h2 className="dark_gray_text text-align-center">Unfortunately, availability is limited for the date and time that you have selected.</h2>
+                    <h2 className="dark_gray_text text-align-center">Please select an alternative or contact us at 1234567890.</h2>
+                    <br/>
+                    <form className="flexbox-col take-the-full-container align-center">
+                        {mappedItems}
+                        <br/>
+                        <div className="flexbox-row align-self-end">
+                            <Button className="myButton secondary" value="Back" link="ReservationOptions" />
+                            <button type="submit" className="myButton primary">Proceed</button>
+                        </div>
+                    </form>
+                </div>
+             </div>
+         </div>
     )
 }
 
--- src/components/ReservationDetails.js
+++ src/components/ReservationDetails.js
@@ -1,37 +1,64 @@
 import Button from "./Button";
+import phoneIcon from "../icons/call_FILL0_wght200_GRAD0_opsz48.svg";
+import commentIcon from "../icons/comment_FILL0_wght200_GRAD0_opsz48.svg";
+import dinnerIcon from "../icons/table_restaurant_FILL0_wght200_GRAD0_opsz48.svg";
+import mailIcon from "../icons/mail_FILL0_wght200_GRAD0_opsz48.svg";
+import personIcon from "../icons/person_FILL0_wght200_GRAD0_opsz48.svg";
+import dietaryRestrIcon from "../icons/no_meals_FILL0_wght200_GRAD0_opsz48.svg";
+import occasionIcon from "../icons/nightlife_FILL0_wght200_GRAD0_opsz48.svg";
 
 function ReservationDetails(props) {
     return (
         <div className="ReservationDetailsSection">
-            <h1>Reservation details</h1>
             <form>
-                <div className="col-left">
-                    {/* I need the onChange functionality for this to work... */}
-                    <input type="text" value="Name" className="myFormInput" id="userName" minLength="2" required />
-                    <input type="text" value="Phone" className="myFormInput" id="userPhone" min="10" required />
-                    <input type="email" value="Email" id="email" required />
-                    <input type="text" value="Dietary Restrictions" className="myFormInput" id="userDiet"/>
+                <div className="flex-container">
+                    <h1 className="dark_gray_text">Reservation details</h1>
+                    <div className="flexbox-row">
+                        <p>4 people</p>
+                        <p>Monday 14th February</p>
+                        <p>21.00</p>
+                    </div>
                 </div>
-                <div className="col-right">
-                    <select name="Seating options" id="seatingOptions">
-                        <option value="Inside">Inside</option>
-                        <option value="Outside">Outside</option>
-                        <option value="Bar">Bar</option>
-                    </select>
 
-                    <select name="Occasion" id="occassionOptions">
-                        <option value="Birthday">Birthday</option>
-                        <option value="Engagement">Engagement</option>
-                        <option value="Anniversary">Anniversary</option>
-                        <option value="Other">Other (Please specify in the comments)</option>
-                    </select>
-                    <textarea value="Type your comments here" className="myFormInput" id="userComment"/>
+                <div className="flex-container space-center">
+                    <div className="flexbox-col take-the-full-container">
+                        {/* I need the onChange functionality for this to work... */}
+                        <input type="text" placeholder="Name" className="myFormInput lead-text light_gray dark_gray_text" id="userName"  style={{ backgroundImage: `url(${personIcon})`}} minLength="2" required />
+                        <input type="text" placeholder="Phone" className="myFormInput lead-text light_gray dark_gray_text" id="userPhone" style={{ backgroundImage: `url(${phoneIcon})`}} min="10" required />
+                        <input type="email" placeholder="Email" className="myFormInput lead-text light_gray dark_gray_text" id="email" style={{ backgroundImage: `url(${mailIcon})`}} required />
+                        <input type="text" placeholder="Dietary Restrictions" className="myFormInput lead-text light_gray dark_gray_text" id="userDiet" style={{ backgroundImage: `url(${dietaryRestrIcon})`}}/>
+                    </div>
+                    <div className="flexbox-col take-the-full-container">
+                        <select name="Seating options" defaultValue="Seating options" className="myFormInput lead-text light_gray dark_gray_text" id="seatingOptions" style={{ backgroundImage: `url(${dinnerIcon})`}}>
+                            <option value="0">Seating options</option>
+                            <option value="Inside">Inside</option>
+                            <option value="Outside">Outside</option>
+                            <option value="Bar">Bar</option>
+                        </select>
+
+                        <select name="Occasion" className="myFormInput lead-text light_gray dark_gray_text" id="occassionOptions" style={{ backgroundImage: `url(${occasionIcon})`}}>
+                            <option value="0">Occasion</option>
+                            <option value="Birthday">Birthday</option>
+                            <option value="Engagement">Engagement</option>
+                            <option value="Anniversary">Anniversary</option>
+                            <option value="Other">Other (Please specify in the comments)</option>
+                        </select>
+                        <textarea placeholder="Type your comments here" className="myFormInput lead-text light_gray dark_gray_text" id="userComment"  style={{ backgroundImage: `url(${commentIcon})`}}/>
+
+                    </div>
                 </div>
-                <input type="checkbox" id="terms" required/>
-                <label htmlFor="terms">By clicking this box, I agree with the Terms of Use and the Privacy Policy of this service.</label>
-                <Button className="myButton secondary" value="Back" link="ReservationOptions" />
-                <button type="submit" className="myButton primary">Confirm</button>
-
+                <div className="flex-container space-center">
+                    <div className="flexbox-row">
+                        <input type="checkbox" id="terms" required />
+                        <label htmlFor="terms" className="dark_gray_text lead-text">By clicking this box, I agree with the Terms of Use and the Privacy Policy of this service.</label>
+                    </div>
+                </div>
+                <div className="flex-container space-end">
+                    <div className="flexbox-row ">
+                        <Button className="myButton secondary" value="Back" link="ReservationOptions" />
+                        <button type="submit" className="myButton primary">Confirm</button>
+                    </div>
+                </div>
             </form>
         </div>
     )
