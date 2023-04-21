2ce1f66b155cdbcd934dda5957b020edde7643db
Dimitra Be <d.berni@hotmail.com>
2023-04-21 14:11:33 +0300

    Add the sidebar and onCLick functionality of the hamburger, the sidebar is an overlay from the right with no animations, make the header sticky in small screens. TBF if the sidebar is open and the screen size changes we have a problem.

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,27 +3,28 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 import Nav from "./Nav";
-
-function clickHamburger(){
-    // const getStatus = document.getElementById("mySidebar").style.display;
-    // if (getStatus == "none"){
-    //     document.getElementById("mySidebar").style.display = 'block';
-    // }else{
-    //     document.getElementById("mySidebar").style.display = 'none';
-    // }
-    console.log('click');
-}
-
+import { useState } from 'react';
 
 function Header(props){
+
+    const [hamburgerClicked, setHamburgerClicked] = useState(false);
+    const [sidebarClass, setSidebarClass] = useState("hidden");
+
+    const clickHamburger = () => {
+        if (!hamburgerClicked){
+            setSidebarClass("visible");
+        }
+        else{
+            setSidebarClass("hidden");
+        }
+        setHamburgerClicked(!hamburgerClicked);
+    }
+
     return(
         <header className="header-container">
             <img src={logo} alt="The little lemon logo consists of a yellow lemon on the left and right next to it, it is written 'Little Lemon' in capital letters"/>
-            <Nav className="flexbox-row dark_gray_text" responsiveness="hide-small hide-medium"/>
-            <a href="#mySidebar" className="hide-large dark_gray_text" onClick={clickHamburger()}>
-                    <FontAwesomeIcon icon={faBars} size="1x"/>
-            </a>
-            <Nav id="mySidebar" className="flexbox-col align-right dark_gray_text animate-right"/>
+            <Nav className="responsiveFlexDir dark_gray_text" responsiveness={`${sidebarClass}`}/>
+            <FontAwesomeIcon icon={faBars} size="1x" className="hide-large dark_gray_text" onClick={clickHamburger}/>
         </header>
     )
 }
