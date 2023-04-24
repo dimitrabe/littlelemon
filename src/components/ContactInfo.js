3bf7c0972eea2734500c2c4df799e2dcad8fbdfc
Dimitra Be <d.berni@hotmail.com>
2023-04-24 19:09:34 +0300

    Changed the contact details from header to p to establish hierarchy and increase readability.

--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -2,9 +2,9 @@ function ContactInfo(props){
     return(
         <ul className={props.className}>
             <li><h3>Contact</h3></li>
-            <li><h3>Imaginary street 12</h3></li>
-            <li><h3>1234567890</h3></li>
-            <li><h3><a href="littlelemon@littlelemon.com">littlelemon@littlelemon.com</a></h3></li>
+            <li><p>Imaginary street 12</p></li>
+            <li><p>1234567890</p></li>
+            <li><p><a href="littlelemon@littlelemon.com">littlelemon@littlelemon.com</a></p></li>
         </ul>
     )
 }
